f51fe9c43f07ba596bac293bbff2f5165d8c0424
juso40 <justin.sostmann@googlemail.com>
2018-11-22 14:51:01 +0100

    ipk ascii stuff

--- IPK/Übung05/Aufgabe2.cc
+++ IPK/Übung05/Aufgabe2.cc
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 int parse_int(std::string number){
-
+  char temp;
+  int n;
+  int numberascii=0;
+  for (int i=0; i<number.size(); i++){
+    temp=number[i];
+    temp=temp-'0';
+    numberascii=numberascii+temp*pow(10,number.size()-i-1);
+  }
+  std::cout<<"Number original: "<<number<<std::endl;
+  std::cout<<"Number after ascii: "<<numberascii<<std::endl;
+  return 0;
 }
 
 int main(){
-std::string s = "47218";
-std::getline(std::cin,s);
-int size = s.size();
-char c = s[0];
-char d = s[size-1];
+  std::cout<<"Enter a Number"<<std::endl;
+  std::string s = "47218";
+  std::getline(std::cin,s);
+  parse_int(s);
 }
