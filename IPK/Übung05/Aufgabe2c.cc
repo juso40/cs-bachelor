af2e085631d53a6bf24e6950ab833c46347d53af
juso40 <justin.sostmann@googlemail.com>
2018-11-24 12:49:33 +0100

    optimized code

--- IPK/Übung05/Aufgabe2c.cc
+++ IPK/Übung05/Aufgabe2c.cc
@@ -1,42 +1,38 @@
 #include <iostream>
 #include <string>
-#include <cmath>
 
 int case_parse_int(std::string number){
   char temp;
   int numberascii=0;
+  int negative=1;
   for (int i=0; i<number.size(); i++){
     temp=number[i];
     switch (temp){
       case ' ':
-      temp=temp-' ';
-      numberascii=numberascii+temp*pow(10,number.size()-i-1);
-      break;
+        break;
       case '+':
-      temp=temp-'+';
-      break;
-      numberascii=numberascii+temp*pow(10,number.size()-i);
+        break;
       case '-':
-      temp=temp-'-';
-      numberascii=numberascii+temp*pow(10,number.size()-i-1);
-      break;
+        negative=-1;
+        break;
       case '0':
       case '1':
       case '2':
       case '3':
       case '4':
-      case '5'://if ts any number then in skips trough all cases till 9
+      case '5'://if its any number then in skips trough all cases till 9
       case '6': //and then calculates
       case '7':
       case '8':
       case '9':
-      temp=temp-'0';
-      numberascii=numberascii+temp*pow(10,number.size()-i-1);
-      break;
+        numberascii=numberascii*10+(temp-'0');
+        break;
       default://if its neither a number, nor a whitespace or a '+' or '-'
-      numberascii=numberascii/pow(10,number.size()-i);//then it stops
-      i=number.size();//stop condition
+        i=number.size();//stop condition
     }
+  }
+  if (negative==-1){
+    numberascii=negative*numberascii;
   }
   std::cout<<"Number original: "<<number<<std::endl;//just to test the correct output of the original and new string/int
   std::cout<<"Number after ascii: "<<numberascii<<std::endl;
