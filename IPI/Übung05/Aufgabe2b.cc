964888890765b0302c950d21a15452548b33ea6f
juso40 <justin.sostmann@googlemail.com>
2018-11-27 12:49:21 +0100

    comments changed

--- IPI/Übung05/Aufgabe2b.cc
+++ IPI/Übung05/Aufgabe2b.cc
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <cmath>
 
-int main(){
-  int n;
+int main()
+{
+  float n;
   std::cout<<"Enter a value for n:"<<std::flush;
   std::cin>>n;
   float a=pow(10,n);
-  float b=pow(-10,n);//test
-  float c=pow(10,-n);
+  float b=-a;
+  float c=1.0/a;
   std::cout.precision(16);
-  std::cout<<"(a+b)+c="<<(a+b)+c<<std::endl;
-  std::cout<<"a+(b+c)="<<a+(b+c)<<std::endl;
-  //not associative with n=3, n=5, n=7, n=9.....
+  std::cout<<"a="<<a<<" b="<<b<<" c="<<c<<std::endl;
+  std::cout<<"(a+b)+c="<<(a+b)+c<<std::endl;//(a+b)=0 everytime -> outputs unrounded c
+  std::cout<<"a+(b+c)="<<a+(b+c)<<std::endl;//(b+c) outputs b, because of too litle digit precision of a float
+  //-> (b+c)=b -> a+b=0
 }
