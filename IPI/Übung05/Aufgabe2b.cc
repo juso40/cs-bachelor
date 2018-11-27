1d8221fea1a99254a05b82e781ec22e41046f054
juso40 <justin.sostmann@googlemail.com>
2018-11-27 11:05:14 +0100

    e

--- IPI/Übung05/Aufgabe2b.cc
+++ IPI/Übung05/Aufgabe2b.cc
@@ -6,7 +6,7 @@ int main(){
   std::cout<<"Enter a value for n:"<<std::flush;
   std::cin>>n;
   float a=pow(10,n);
-  float b=pow(-10,n);
+  float b=pow(-10,n);//test
   float c=pow(10,-n);
   std::cout.precision(16);
   std::cout<<"(a+b)+c="<<(a+b)+c<<std::endl;
