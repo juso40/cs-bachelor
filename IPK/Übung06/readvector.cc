5fe19e82bb1d8d527cea31cf1081791743875da1
juso40 <justin.sostmann@googlemail.com>
2018-11-29 11:15:58 +0100

    median done

--- IPK/Übung06/readvector.cc
+++ IPK/Übung06/readvector.cc
@@ -2,12 +2,13 @@
 #include <vector>
 #include "io.hh"
 #include "statistics.hh"
+#include "statistics.cc"
+#include "io.cc"
 
 int main(){
 	std::vector<double> rv=read_vector();
 	for (auto i:rv){
 		std::cout<<i<<std::endl;
-	}	
-	std::cout<<median(rv)<<std::endl;
+	}
+	std::cout<<"Median"<<median(rv)<<std::endl;
 }
-
--- IPK/Übung06/statistics.cc
+++ IPK/Übung06/statistics.cc
@@ -1,5 +1,6 @@
 #include <vector>
 #include <algorithm>
+#include <cmath>
 #include "statistics.hh"
 
 double mean(const std::vector<double>& v){
@@ -14,7 +15,11 @@ double median(const std::vector<double>& v){
     std::vector<double> vsort=v;
     std::sort(vsort.begin(), vsort.end());
     double mdian;
-    
-    
+    if ((vsort.size()+1)%2!=0){
+      mdian=vsort[(vsort.size()+1)/2];
+    }
+    if ((vsort.size()+1)%2==0){
+      mdian=vsort[(vsort.size()/2+(vsort.size()/2+1))/2];
+    }
     return mdian;
 }
