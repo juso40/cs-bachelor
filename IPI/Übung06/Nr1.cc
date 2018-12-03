1a16a5faf7c786509f509028e2addfb1cc3bf150
juso40 <justin.sostmann@googlemail.com>
2018-12-03 23:09:22 +0100

    smol change

--- IPI/Übung06/Nr1.cc
+++ IPI/Übung06/Nr1.cc
@@ -2,12 +2,15 @@
 
 double root_analog_to_lecture(double base) {
     double result, approximation = 2.;
+    int iterations=0;
     while(true) {
-        result = (base / approximation + approximation) / 2;
-        if(result == approximation)
-          break;
-        approximation = result;
+      iterations++;
+      result = (base / approximation + approximation) / 2;
+      if(result == approximation)
+        break;
+      approximation = result;
     }
+    std::cout << "Approximated result after: " << iterations << " iteration(s). Is: ";
     return result;
 }
 
@@ -16,6 +19,6 @@ int main() {
 
     std::cout << "Square Root of: " << std::flush;
     std::cin >> base;
-    std::cout << "\nSquare root is: " << root_analog_to_lecture(base)<<std::endl;
+    std::cout << root_analog_to_lecture(base)<<std::endl;
     return 0;
 }
