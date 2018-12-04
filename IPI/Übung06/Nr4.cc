2f6f2bd5b39b1e294c998cd9f2eed7f2cac6aadc
juso40 <justin.sostmann@googlemail.com>
2018-12-04 14:31:26 +0100

    help

--- IPI/Übung06/Nr4.cc
+++ IPI/Übung06/Nr4.cc
@@ -2,61 +2,54 @@
 #include <iostream>
 #include <cmath>
 
-bool deck_check(int deck[], int n){
-  int deck_real[n];
+bool deck_check(int deck_shuffled[], int n){
   for (unsigned i=0;i<n;i++){
-    deck_real[i]=i;
-    if (deck_real[i]!=deck[i]){
+    if (i!=deck_shuffled[i]){
+      std::cout<<"False!\n\n\n"<<std::endl;
       return false;
     }
   }
+  std::cout<<"\n\nTrue!\n\n"<<std::endl;
   return true;
 }
 
-int perfect_out(int deck_out[], int n){
+void perfect_out(int deck[], int n){
   int half1[n/2];
   int half2[n/2];
   for (unsigned i=0;i<n/2;i++){
     half1[i]=i;
     half2[i]=n/2+i;
   }
-  int counter=0;
-  int counter_small=0;
+
   int steps=0;
   while (true){
-    deck_out[counter_small*2]=half1[counter_small];
-    deck_out[counter_small*2+1]=half2[counter_small];
+    for (int counter=0;counter<n/2;counter++){
+      deck[(counter*2)]=half1[counter];
+      deck[((counter*2)+1)]=half2[counter];
 
-    half1[counter_small]=deck_out[counter_small];
-    half2[counter_small]=deck_out[n/2+counter_small];
+      half1[counter]=deck[counter];
+      half2[counter]=deck[((n/2)+counter)];
 
-    std::cout<<"perfect-out: "<<deck_out[counter]<<"| card number: "<<counter<<std::endl;
-    counter++;
-    counter_small++;
-
-    if ((counter_small%(n/2))==0){
-      counter_small=0;
     }
-    if ((counter%n)==0){
-      counter=0;
-    }
-    //break condition
-    if (deck_check(deck_out, n)==true){
-      break;
+    for (int out=0; out<n; out++){
+        std::cout<<"perfect-out: "<<deck[out]<<"| card number: "<<out<<std::endl;
     }
     steps++;
+    //break condition
+    if (deck_check(deck, n)==true){
+      break;
+    }
   }
-    std::cout<<"No. of steps:"<<steps/n<<std::endl;
-    return 0;
+  std::cout<<"Number of steps: "<<steps<<std::endl;
 }
 
 int main(){
-  int n=52;
+  int n=4;
   int deck[n];
 
   for (unsigned i=0;i<n;i++){
     deck[i]=i;
   }
   perfect_out(deck, n);
-  //perfect_out(deck);
+  return 0;
 }
