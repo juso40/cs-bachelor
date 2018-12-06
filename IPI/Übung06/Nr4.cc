d8cdcdc61a380c8d5b755a5aa6c782b9aa93796f
juso40 <justin.sostmann@googlemail.com>
2018-12-06 11:56:38 +0100

    deleted old nr4

--- IPI/Übung06/Nr4.cc
+++ IPI/Übung06/Nr4.cc
@@ -1,55 +1,76 @@
 #include <array>
 #include <iostream>
-#include <cmath>
 
 bool deck_check(int deck_shuffled[], int n){
-  for (unsigned i=0;i<n;i++){
+  for (int i=0;i<n;i++){    //compares my shuffled deck to the original deck
     if (deck_shuffled[i]!=i){
       std::cout<<"False!\n\n\n"<<std::endl;
-      return false;
-    }
+      return false;   //as long as my shuffled deck doesent equal my original
+    }                  //it returns flase -> loop continous
   }
   std::cout<<"\n\nTrue!\n\n"<<std::endl;
   return true;
 }
 
-void perfect_out(int deck[], int n){
-  int half1[n/2];
-  int half2[n/2];
-  for (unsigned i=0;i<n/2;i++){
-    half1[i]=i;
-    half2[i]=n/2+i;
-  }
 
-  int steps=0;
-  while (true){
-    for (int counter=0;counter<n/2;++counter){
-      deck[(counter*2)]=half1[counter];
-      deck[((counter*2)+1)]=half2[counter];
 
-      half1[counter]=deck[counter];
-      half2[counter]=deck[((n/2)+counter)];
 
-    }
-    for (int out=0; out<n; ++out){
-        std::cout<<"perfect-out: "<<deck[out]<<"| card number: "<<out<<std::endl;
-    }
-    ++steps;
-    //break condition
-    if (deck_check(deck, n)==true){
-      break;
-    }
-  }
-  std::cout<<"Number of steps: "<<steps<<std::endl;
-}
 
 int main(){
-  int n=4;
-  int deck[n];
+  int n = 52;     //number of cards in my deck
+  int deck[n];    //my original deck
+  int half1[n/2];//first half of my deck
+  int half2[n/2];//second half of my deck
+  bool first_shuffle = true;
 
-  for (unsigned i=0;i<n;++i){
-    deck[i]=i;
+  for (int i=0; i<n; i++){
+    deck[i]=i;    //fills my deck with the original values
   }
-  perfect_out(deck, n);
-  return 0;
+
+  int counter=0;//to count my amount i shuffled the deck
+  //perfect out shuffle
+  while ((first_shuffle == true) || (deck_check(deck, n)==false)){ //self explanatory
+    for (int i=0; i<n/2; i++){  //splits my deck into 2 seperate halfes
+      half1[i]=deck[i];
+      half2[i]=deck[(n/2)+i];
+    }
+    for (int i=0; i<n/2; i++){//shuffles my 2 seperate halfs into one shuffled deck
+      deck[i*2]=half1[i];
+      deck[i*2+1]=half2[i];
+    }
+    //optional, just for visuals
+    for (int i=0; i<n; i++){
+      std::cout<<i<<". card is: "<<deck[i]<<"\n";
+    }
+    first_shuffle=false;
+    counter++;//counts number of shuffles
+  }
+  //outputs 8 with a standard deck of 52 cards
+  std::cout << "Number of perfect out shuffles required: "<< counter << std::endl;
+
+  //resets my conditions
+  counter=0;//to count my amount i shuffled the deck
+  for (int i=0; i<n; i++){
+    deck[i]=i;    //fills my deck with the original values
+  }
+  first_shuffle = true;
+  //perfect in shuffle
+  while ((first_shuffle == true) || (deck_check(deck, n)==false)){ //self explanatory
+    for (int i=0; i<n/2; i++){  //splits my deck into 2 seperate halfes
+      half1[i]=deck[i];
+      half2[i]=deck[(n/2)+i];
+    }
+    for (int i=0; i<n/2; i++){//shuffles my 2 seperate halfs into one shuffled deck
+      deck[i*2+1]=half1[i]; //opposit of perfect out shuffle
+      deck[i*2]=half2[i];
+    }
+    //optional, just for visuals
+    for (int i=0; i<n; i++){
+      std::cout<<i<<". card is: "<<deck[i]<<"\n";
+    }
+    first_shuffle=false;
+    counter++;//counts number of shuffles
+  }
+  //outputs 52 with a standard deck of 52
+  std::cout << "Number of perfect in shuffles required: " << counter << std::endl;
 }
