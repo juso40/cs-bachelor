69837c9a526cfca05e276db3146bfcc86d96a630
juso40 <justin.sostmann@googlemail.com>
2019-01-16 11:13:29 +0100

    done

--- IPI/Übung10/Nr1/Fehlerrechnung.cc
+++ IPI/Übung10/Nr1/Fehlerrechnung.cc
@@ -9,7 +9,7 @@ FehlerWert::FehlerWert()
 FehlerWert::FehlerWert(const double& wert,const double& fehler)
 :_wert(wert), _fehler(fehler)
 {}
-
+//berechnung des fehlerwerts der summe
 FehlerWert FehlerWert::operator+(const FehlerWert& other)
 {
   FehlerWert Summe;
@@ -17,15 +17,16 @@ FehlerWert FehlerWert::operator+(const FehlerWert& other)
   Summe._fehler=sqrt(pow(_fehler,2)+pow(other._fehler,2));
   return Summe;
 }
-
+//berechnung des fehlerwerts des produkts
 FehlerWert FehlerWert::operator*(const FehlerWert& other)
 {
   FehlerWert Produkt;
   Produkt._wert=_wert*other._wert;
+  //Formeln waren uns gegeben, trivial(einfach)
   Produkt._fehler=(sqrt(pow((_fehler/_wert),2)+pow((other._fehler/other._wert),2)))*Produkt._wert;
   return Produkt;
 }
-
+//accsessor methoden
 double FehlerWert::wert()
 {
   return _wert;
