174f770a66d03011ef133cd4456a176f7ac699ed
juso40 <justin.sostmann@googlemail.com>
2019-02-08 15:51:29 +0100

    looks better now

--- Übungen/test.cc
+++ Übungen/test.cc
@@ -2,9 +2,20 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <locale>
 
+void str_replacer(std::string &line, char vocal){
+  for (int i=0; i<line.size(); i++){
+    if (line[i]== 'a'| line[i]== 'e'| line[i]== 'i'| line[i]== 'o'| line[i]== 'u'){
+      line[i]=std::tolower(vocal, std::locale());
+    }
+    if (line[i]== 'A'| line[i]== 'E'| line[i]== 'I'| line[i]== 'O'| line[i]== 'U'){
+      line[i]=std::toupper(vocal, std::locale());
+    }
+  }
+}
 
-void fhandler(std::string title){
+void fhandler(std::string title,char vocal){
   std::string line;
   std::ifstream myfile;
   std::ofstream newfile ("output.txt");
@@ -12,11 +23,7 @@ void fhandler(std::string title){
 
   if(myfile.is_open()){
     while(std::getline(myfile, line)){
-      std::replace(line.begin(), line.end(), 'a', 'e' );
-      std::replace(line.begin(), line.end(), 'e', 'e' );
-      std::replace(line.begin(), line.end(), 'i', 'e' );
-      std::replace(line.begin(), line.end(), 'o', 'e' );
-      std::replace(line.begin(), line.end(), 'u', 'e' );
+      str_replacer(line, vocal);
       std::cout<<line<<std::endl;
       newfile<<line<<std::endl;;
     }
@@ -24,16 +31,19 @@ void fhandler(std::string title){
   else{
     std::cout<<"Couln't open file."<<std::endl;
   }
+  myfile.close();
+  newfile.close();
 }
-
-
 
 int main()
 {
+  char vocal;
   std::string title;
   std::cout << "Enter files name with the extension. " << std::flush;
   std::cin >> title;
-  fhandler(title);
+  std::cout << "Enter a char to replace all vocals with. " << std::flush;
+  std::cin >> vocal;
+  fhandler(title, vocal);
 
   return 0;
 }
